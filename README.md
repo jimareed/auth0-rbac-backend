ed3e5e4fb6130487a8e8937283602bf4ba138202
jimareed <jim.a.reed@gmail.com>
2020-11-24 19:11:22 -0500

    added backend
    added backend

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # auth0-rbac-backend
+Basic RBAC implimentation using React, Go and Auth0. Also requires https://github.com/jimareed/auth0-rbac. Based on https://github.com/jimareed/react-auth0-login, the samples from Auth0 and the Auth0 blog: https://auth0.com/blog/authentication-in-golang/.
+
+Setup:
+1. follow the setup instructions in: https://github.com/jimareed/auth0-rbac
+2.  Update your `main.go` file with the required values from the Auth0 dashboard. There are 3 different spots that need to be updated:
+
+```go
+// main.go
+// ...
+aud := "YOUR_API_IDENTIFIER"
+// ...
+iss := "https://YOUR_DOMAIN/"
+// ...  
+resp, err := http.Get("https://YOUR_DOMAIN/.well-known/jwks.json")
+// ...
+```
+
+Your **Identifier** is listed on the Settings tab of the API you just created in the dashboard. To find `YOUR_DOMAIN`, click on the **Quick Start** tab and scroll down to the code snippet. Copy the value for `options.Authority` and use that to replace `YOUR_DOMAIN`. Make sure you keep the trailing slash for `iss` in `main.go`.
+
+**Run Go**
+
+* In a separate terminal tab, grab the dependencies
+
+```bash
+go get
+```
+
+* Build and run your Go application:
+
+```bash
+go run .
+```
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+// Import our dependencies. We'll use the standard HTTP library as well as the gorilla router for this app
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
+
+/* Apps type */
+type App struct {
+	Id          int
+	Name        string
+	Description string
+}
+
+var apps = []App{
+	App{Id: 1, Name: "App 1", Description: "App 1 description"},
+	App{Id: 2, Name: "App 2", Description: "App 2 description"},
+	App{Id: 3, Name: "App 3", Description: "App 3 description"},
+}
+
+func main() {
+
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			// Verify 'aud' claim
+			aud := "YOUR_API_IDENTIFIER"
+			checkAud := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAud {
+				return token, errors.New("Invalid audience.")
+			}
+			// Verify 'iss' claim
+			iss := "https://YOUR_DOMAIN/"
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("Invalid issuer.")
+			}
+
+			cert, err := getPemCert(token)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+			return result, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+
+	r := mux.NewRouter()
+
+	r.Handle("/apps", jwtMiddleware.Handler(AppsHandler)).Methods("GET")
+
+	// For dev only - Set up CORS so React client can consume our API
+	corsWrapper := cors.New(cors.Options{
+		AllowedMethods: []string{"GET", "POST"},
+		AllowedHeaders: []string{"Content-Type", "Origin", "Accept", "*"},
+	})
+
+	http.ListenAndServe(":8080", corsWrapper.Handler(r))
+}
+
+var AppsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	payload, _ := json.Marshal(apps)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
+})
+
+func getPemCert(token *jwt.Token) (string, error) {
+
+	cert := ""
+	resp, err := http.Get("https://YOUR-DOMAIN.com/.well-known/jwks.json")
+
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	for k, _ := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		err := errors.New("Unable to find appropriate key.")
+		return cert, err
+	}
+
+	return cert, nil
+}
